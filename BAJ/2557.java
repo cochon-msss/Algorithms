88551af4791bb67f64d8a7905c89262574b591e4
손민성 <son344910@naver.com>
2025-05-13 20:56:21 +0900

    백준 알고리즘 추가 테스트

---
+++ BAJ/2557.java
@@ -0,0 +1,9 @@
+# 링크 : https://www.acmicpc.net/problem/2557
+# 시간 : 108ms
+# 메모리 : 13876KB
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
