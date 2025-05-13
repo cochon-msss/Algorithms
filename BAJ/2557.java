7a2ac1c08a61e88ffe95c6d006d0aa0adb9d3f1a
cochon-msss <115930703+cochon-msss@users.noreply.github.com>
2025-05-13 20:57:37 +0900

    Update 2557.java

--- BAJ/2557.java
+++ BAJ/2557.java
@@ -1,6 +1,6 @@
-# 링크 : https://www.acmicpc.net/problem/2557
-# 시간 : 108ms
-# 메모리 : 13876KB
+// 링크 : https://www.acmicpc.net/problem/2557
+// 시간 : 108ms
+// 메모리 : 13876KB
 
 public class Main {
     public static void main(String[] args) {
